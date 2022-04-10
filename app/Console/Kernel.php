b44bf10f730ebf8be6c7ad5546cd81607e7ee114
ricky bien <guitarbien@gmail.com>
2022-04-10 15:20:46 +0000

    change schedule to daily

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('test:me')
-                 ->hourly()
+                 ->daily()
                  ->onSuccess(function () {
                      \Log::info('scheduled success');
                  })
